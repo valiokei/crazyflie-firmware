5530d0113eca92c9d49c26ef4a9cf1d609840e99
valiokei <valiokei@gmail.com>
2024-06-03 15:53:56 +0200

    Adding simulation Volts file. Some bug Fixing in the measurment model
    Removed the normalization of the magnetic filed. Looking at the values of the Volts, they don't make any sense using the normalization.
    Also, following this, the std dvt of the magnetic measurements for the kalman filter was increased .

--- src/modules/src/kalman_core/mm_magnetic_distance.c
+++ src/modules/src/kalman_core/mm_magnetic_distance.c
@@ -1,12 +1,16 @@
 #include "math.h"
 #include "mm_magnetic_distance.h"
 #include "log.h"
+#include "Volts_500_4_lut.h"
+#include "debug.h"
+#include "param.h"
+#include <stdlib.h>
 
 #define G_INA 1000.0f
 #define RAY 0.019f
 #define N_WOUNDS 5.0f
 #define COIL_SURFACE (RAY * RAY * PI)
-#define CURRENT 1.5f
+#define CURRENT 0.5f
 #define MU_0 1.25663706e-6f
 
 #define start_timer() *((volatile uint32_t *)0xE0001000) = 0x40000001 // Enable CYCCNT register
@@ -23,16 +27,52 @@ float32_t B[4][3] = {{0.0f, 0.0f, 0.0f},
                      {0.0f, 0.0f, 0.0f},
                      {0.0f, 0.0f, 0.0f},
                      {0.0f, 0.0f, 0.0f}};
+float32_t E_1 = 0.0f;
+float32_t E_2 = 0.0f;
+float32_t E_3 = 0.0f;
+float32_t E_4 = 0.0f;
 
-// float sign(float x)
-// {
-//     if (x > 0)
-//         return 1.0f;
-//     else if (x < 0)
-//         return -1.0f;
-//     else
-//         return 0.0f;
-// }
+float32_t T_pre_x = 0.0f;
+float32_t T_pre_y = 0.0f;
+float32_t T_pre_z = 0.0f;
+
+volatile uint16_t CG_a1 = 1.3f;   //  NERO
+volatile uint16_t CG_a2 = 0.635f; // GIALLO
+volatile uint16_t CG_a3 = 3.5f;   // GRIGIO
+volatile uint16_t CG_a4 = 7.5f;   // ROSSO
+
+float32_t CalibrationRapport_anchor1 = 1.0f;
+float32_t CalibrationRapport_anchor2 = 1.0f;
+float32_t CalibrationRapport_anchor3 = 1.0f;
+float32_t CalibrationRapport_anchor4 = 1.0f;
+
+volatile uint32_t pos_idx = 0;
+
+// DEBUG FUNCTION
+double generate_gaussian_noise(double mu, double sigma)
+{
+    static const double epsilon = 1e-10;
+    static const double two_pi = 2.0 * 3.14159265358979323846;
+
+    static double z1;
+    static int generate;
+    generate = !generate;
+
+    if (!generate)
+        return z1 * sigma + mu;
+
+    double u1, u2;
+    do
+    {
+        u1 = rand() / (RAND_MAX + 1.0);
+        u2 = rand() / (RAND_MAX + 1.0);
+    } while (u1 <= epsilon);
+
+    double z0;
+    z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
+    z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
+    return z0 * sigma + mu;
+}
 
 float dot_product(float *a, float *b, int length)
 {
@@ -566,12 +606,17 @@ float get_B_field_for_a_Anchor(float *anchor_pos,
 {
     float tx_rx_versor[3];
     getversor(anchor_pos, tag_pos, tx_rx_versor, 3);
+    // DEBUG_PRINT("tx_rx_versor = %f\n", tx_rx_versor);
 
     float magnetic_dipole_moment_tx[3] = {
         N_WOUNDS * COIL_SURFACE * CURRENT * tag_or_versor[0],
         N_WOUNDS * COIL_SURFACE * CURRENT * tag_or_versor[1],
         N_WOUNDS * COIL_SURFACE * CURRENT * tag_or_versor[2]};
+    // DEBUG_PRINT("magnetic_dipole_moment_tx = %f\n", magnetic_dipole_moment_tx);
+
     float tx_rx_distance = euclidean_distance(anchor_pos, tag_pos, 3);
+
+    // DEBUG_PRINT("tx_rx_distance = %f\n", tx_rx_distance);
 
     float dot_product_B_temp = dot_product(magnetic_dipole_moment_tx, tx_rx_versor, 3);
 
@@ -581,18 +626,24 @@ float get_B_field_for_a_Anchor(float *anchor_pos,
         constant_Bfield_constant_1 * (3 * dot_product_B_temp * tx_rx_versor[1] - magnetic_dipole_moment_tx[1]),
         constant_Bfield_constant_1 * (3 * dot_product_B_temp * tx_rx_versor[2] - magnetic_dipole_moment_tx[2])};
 
-    float magnetic_dipole_moment_tx_magnitude = euclidean_distance(magnetic_dipole_moment_tx, magnetic_dipole_moment_tx, 3);
+    // float magnetic_dipole_moment_tx_magnitude = euclidean_distance(magnetic_dipole_moment_tx, magnetic_dipole_moment_tx, 3);
+    // compute the norm of the magnetic dipole moment
+    float magnetic_dipole_moment_tx_magnitude = sqrtf(magnetic_dipole_moment_tx[0] * magnetic_dipole_moment_tx[0] +
+                                                      magnetic_dipole_moment_tx[1] * magnetic_dipole_moment_tx[1] +
+                                                      magnetic_dipole_moment_tx[2] * magnetic_dipole_moment_tx[2]);
 
-    B_field[0] = B_temp[0] * magnetic_dipole_moment_tx_magnitude,
-    B_field[1] = B_temp[1] * magnetic_dipole_moment_tx_magnitude,
-    B_field[2] = B_temp[2] * magnetic_dipole_moment_tx_magnitude;
+    // normalizing the B field
+    B_field[0] = B_temp[0]; // * magnetic_dipole_moment_tx_magnitude;
+    B_field[1] = B_temp[1]; // * magnetic_dipole_moment_tx_magnitude;
+    B_field[2] = B_temp[2]; // * magnetic_dipole_moment_tx_magnitude;
 }
 
 float V_from_B(float *B_field, float *rx_versor, int resonanceFreq)
 {
 
     float dot_product_V = dot_product(B_field, rx_versor, 3);
-    float V = G_INA * fabs(2 * M_PI * resonanceFreq * COIL_SURFACE * N_WOUNDS * dot_product_V);
+
+    float V = G_INA * fabs(2 * PI * resonanceFreq * PI * RAY * RAY * N_WOUNDS * dot_product_V);
     return V;
 }
 
@@ -602,16 +653,24 @@ float kalmanCoreUpdateWithVolt(kalmanCoreData_t *this,
 
     start_timer();     // start the timer.
     it1 = get_timer(); // store current cycle-count in a local
-    MeasuredVoltages[0] = voltAnchor->measuredVolt[0];
-    MeasuredVoltages[1] = voltAnchor->measuredVolt[1];
-    MeasuredVoltages[2] = voltAnchor->measuredVolt[2];
-    MeasuredVoltages[3] = voltAnchor->measuredVolt[3];
 
     // computing the B field for each of the 4 anchors
     float tag_pos_predicted[3] = {this->S[KC_STATE_X], this->S[KC_STATE_Y], this->S[KC_STATE_Z]};
+    // SEMBRA OK
+    T_pre_x = tag_pos_predicted[0];
+    T_pre_y = tag_pos_predicted[1];
+    T_pre_z = tag_pos_predicted[2];
 
     // sarebbe il prodotto tra la matrice di rotazione e il versore  [0,0,1] iniziale
     float tag_or_versor[3] = {this->R[0][2], this->R[1][2], this->R[2][2]};
+
+    // // SEMBRA OK [0,0,1]
+    // // if (pos_idx % 10 == 0)
+    // // {
+    // //     DEBUG_PRINT("tag_or_versor = %f,", tag_or_versor[0]);
+    // //     DEBUG_PRINT("%f,", tag_or_versor[1]);
+    // //     DEBUG_PRINT("%f\n", tag_or_versor[2]);
+    // // }
 
     float B_field_vector_1[3];
     float B_field_vector_2[3];
@@ -623,10 +682,87 @@ float kalmanCoreUpdateWithVolt(kalmanCoreData_t *this,
     float anchor_3_pose[3] = {voltAnchor->x[2], voltAnchor->y[2], voltAnchor->z[2]};
     float anchor_4_pose[3] = {voltAnchor->x[3], voltAnchor->y[3], voltAnchor->z[3]};
 
+    // // ########################################### DEBUG
+    // anchor_1_pose[0] = 0.5;
+    // anchor_1_pose[1] = 0.5;
+    // anchor_1_pose[2] = 0;
+
+    // anchor_2_pose[0] = -0.5;
+    // anchor_2_pose[1] = 0.5;
+    // anchor_2_pose[2] = 0;
+
+    // anchor_3_pose[0] = 0.5;
+    // anchor_3_pose[1] = -0.5;
+    // anchor_3_pose[2] = 0;
+
+    // anchor_4_pose[0] = -0.5;
+    // anchor_4_pose[1] = -0.5;
+    // anchor_4_pose[2] = 0;
+
+    // voltAnchor->x[0] = anchor_1_pose[0];
+    // voltAnchor->y[0] = anchor_1_pose[1];
+    // voltAnchor->z[0] = anchor_1_pose[2];
+
+    // voltAnchor->x[1] = anchor_2_pose[0];
+    // voltAnchor->y[1] = anchor_2_pose[1];
+    // voltAnchor->z[1] = anchor_2_pose[2];
+
+    // voltAnchor->x[2] = anchor_3_pose[0];
+    // voltAnchor->y[2] = anchor_3_pose[1];
+    // voltAnchor->z[2] = anchor_3_pose[2];
+
+    // voltAnchor->x[3] = anchor_4_pose[0];
+    // voltAnchor->y[3] = anchor_4_pose[1];
+    // voltAnchor->z[3] = anchor_4_pose[2];
+
+    // // CHECK THE ASSIGNEMENT WORKED
+    // // DEBUG_PRINT("voltAnchor->x[0] = %f\n", voltAnchor->x[0]);
+
+    // // add noise on the measurments
+    // voltAnchor->measuredVolt[0] = Volts_500_4_lut[pos_idx][0]; // + generate_gaussian_noise(0.0, voltAnchor->stdDev[0]);
+    // voltAnchor->measuredVolt[1] = Volts_500_4_lut[pos_idx][1]; // + generate_gaussian_noise(0.0, voltAnchor->stdDev[1]);
+    // voltAnchor->measuredVolt[2] = Volts_500_4_lut[pos_idx][2]; // + generate_gaussian_noise(0.0, voltAnchor->stdDev[2]);
+    // voltAnchor->measuredVolt[3] = Volts_500_4_lut[pos_idx][3]; // + generate_gaussian_noise(0.0, voltAnchor->stdDev[3]);
+
+    // // DEBUG_PRINT("voltAnchor->measuredVolt[0] = %f\n", voltAnchor->measuredVolt[0]);
+    // pos_idx = pos_idx + 1;
+    // // if (pos_idx % 10 == 0)
+    // // {
+    // //     DEBUG_PRINT("pos_idx = %d\n", pos_idx);
+    // // }
+    // // ########################################### DEBUG
+
+    MeasuredVoltages[0] = voltAnchor->measuredVolt[0] / CG_a1;
+    MeasuredVoltages[1] = voltAnchor->measuredVolt[1] / CG_a2;
+    MeasuredVoltages[2] = voltAnchor->measuredVolt[2] / CG_a3;
+    MeasuredVoltages[3] = voltAnchor->measuredVolt[3] / CG_a4;
+
     get_B_field_for_a_Anchor(anchor_1_pose, tag_pos_predicted, tag_or_versor, B_field_vector_1);
     get_B_field_for_a_Anchor(anchor_2_pose, tag_pos_predicted, tag_or_versor, B_field_vector_2);
     get_B_field_for_a_Anchor(anchor_3_pose, tag_pos_predicted, tag_or_versor, B_field_vector_3);
     get_B_field_for_a_Anchor(anchor_4_pose, tag_pos_predicted, tag_or_versor, B_field_vector_4);
+
+    B[0][0] = B_field_vector_1[0];
+    B[0][1] = B_field_vector_1[1];
+    B[0][2] = B_field_vector_1[2];
+    // if (pos_idx % 10 == 0)
+    // {
+    //     DEBUG_PRINT("B_ANCHOR_0 = %f,", B[0][0] * 10e12);
+    //     DEBUG_PRINT("%f,", B[0][1] * 10e12);
+    //     DEBUG_PRINT("%f\n", B[0][2] * 10e10);
+    // }
+
+    B[1][0] = B_field_vector_2[0];
+    B[1][1] = B_field_vector_2[1];
+    B[1][2] = B_field_vector_2[2];
+
+    B[2][0] = B_field_vector_3[0];
+    B[2][1] = B_field_vector_3[1];
+    B[2][2] = B_field_vector_3[2];
+
+    B[3][0] = B_field_vector_4[0];
+    B[3][1] = B_field_vector_4[1];
+    B[3][2] = B_field_vector_4[2];
 
     // computing the V_rx for each of the 4 anchors
     float V_rx_1 = V_from_B(B_field_vector_1, tag_or_versor, voltAnchor->resonanceFrequency[0]);
@@ -704,16 +840,44 @@ float kalmanCoreUpdateWithVolt(kalmanCoreData_t *this,
     h_4[KC_STATE_X] = V_rx_derivative_x[4];
     h_4[KC_STATE_Y] = V_rx_derivative_y[4];
     h_4[KC_STATE_Z] = V_rx_derivative_z[4];
+
+    float error_anchor1 = voltAnchor->measuredVolt[0] - V_rx_1;
+    float error_anchor2 = voltAnchor->measuredVolt[1] - V_rx_2;
+    float error_anchor3 = voltAnchor->measuredVolt[2] - V_rx_3;
+    float error_anchor4 = voltAnchor->measuredVolt[3] - V_rx_4;
+    E_1 = error_anchor1;
+    E_2 = error_anchor2;
+    E_3 = error_anchor3;
+    E_4 = error_anchor4;
+
+    CalibrationRapport_anchor1 = MeasuredVoltages[0] / PredictedVoltages[0];
+    CalibrationRapport_anchor2 = MeasuredVoltages[1] / PredictedVoltages[1];
+    CalibrationRapport_anchor3 = MeasuredVoltages[2] / PredictedVoltages[2];
+    CalibrationRapport_anchor4 = MeasuredVoltages[3] / PredictedVoltages[3];
+
     // Potrebbe essere troppo piccolo l'errore. o le stime troppo diverse?!?!?!
-    kalmanCoreScalarUpdate(this, &H_1, voltAnchor->measuredVolt[0] - V_rx_1, voltAnchor->stdDev[0]);
-    kalmanCoreScalarUpdate(this, &H_2, voltAnchor->measuredVolt[1] - V_rx_2, voltAnchor->stdDev[1]);
-    kalmanCoreScalarUpdate(this, &H_3, voltAnchor->measuredVolt[2] - V_rx_3, voltAnchor->stdDev[2]);
-    kalmanCoreScalarUpdate(this, &H_4, voltAnchor->measuredVolt[3] - V_rx_4, voltAnchor->stdDev[3]);
+    // kalmanCoreScalarUpdate(this, &H_1, error_anchor1, voltAnchor->stdDev[0]);
+    // kalmanCoreScalarUpdate(this, &H_2, error_anchor2, voltAnchor->stdDev[1]);
+    // kalmanCoreScalarUpdate(this, &H_3, error_anchor3, voltAnchor->stdDev[2]);
+    // kalmanCoreScalarUpdate(this, &H_4, error_anchor4, voltAnchor->stdDev[3]);
 }
 
 LOG_GROUP_START(Dipole_Model)
 
 // LOG_ADD(LOG_UINT32, CPUCycle, &it2)
+LOG_ADD(LOG_FLOAT, C_R_1, &CalibrationRapport_anchor1)
+LOG_ADD(LOG_FLOAT, C_R_2, &CalibrationRapport_anchor2)
+LOG_ADD(LOG_FLOAT, C_R_3, &CalibrationRapport_anchor3)
+LOG_ADD(LOG_FLOAT, C_R_4, &CalibrationRapport_anchor4)
+
+LOG_ADD(LOG_FLOAT, T_pred_x, &T_pre_x)
+LOG_ADD(LOG_FLOAT, T_pred_y, &T_pre_y)
+LOG_ADD(LOG_FLOAT, T_pred_z, &T_pre_z)
+
+LOG_ADD(LOG_FLOAT, Er_1, &E_1)
+LOG_ADD(LOG_FLOAT, Er_2, &E_2)
+LOG_ADD(LOG_FLOAT, Er_3, &E_3)
+LOG_ADD(LOG_FLOAT, Er_4, &E_4)
 
 LOG_ADD(LOG_FLOAT, M_V_0, &MeasuredVoltages[0])
 LOG_ADD(LOG_FLOAT, M_V_1, &MeasuredVoltages[1])
@@ -746,3 +910,10 @@ LOG_ADD(LOG_FLOAT, B_3_2, &B[3][2])
 LOG_ADD(LOG_FLOAT, B_3_3, &B[3][3])
 
 LOG_GROUP_STOP(Dipole_Model)
+
+PARAM_GROUP_START(D_M_P)
+PARAM_ADD(PARAM_UINT16, ga1, &CG_a1)
+PARAM_ADD(PARAM_UINT16, ga2, &CG_a2)
+PARAM_ADD(PARAM_UINT16, ga3, &CG_a3)
+PARAM_ADD(PARAM_UINT16, ga4, &CG_a4)
+PARAM_GROUP_STOP(D_M_P)
